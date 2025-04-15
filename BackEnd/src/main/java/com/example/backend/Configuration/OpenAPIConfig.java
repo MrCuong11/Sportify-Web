542d39c2b388e9ceb559cc3edc64aa695e161668
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-04-15 11:47:59 +0700

    fix swagger call API with jwt, invalid date change to 36000s (for test)

--- BackEnd/src/main/java/com/example/backend/Configuration/OpenAPIConfig.java
+++ BackEnd/src/main/java/com/example/backend/Configuration/OpenAPIConfig.java
@@ -1,16 +1,20 @@
 package com.example.backend.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.OpenAPI;
 
-
-//http://localhost:8080/swagger-ui/index.html
 @Configuration
 public class OpenAPIConfig {
+
+    //http://localhost:8080/swagger-ui/index.html
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -25,7 +29,17 @@ public class OpenAPIConfig {
                                 .url(""))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("http://springdoc.org")));
+                                .url("http://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME)) // Thêm dòng này
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                        )
+                );
     }
-
 }
